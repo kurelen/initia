13fafc5d9b484b2d3337ea9302c4bdee43e2d8af
Alexander Jandt <kurelen@pm.me>
2025-07-28 17:41:06 +0200

    Add testset notebook

---
+++ notebooks/testsets.clj
@@ -0,0 +1,23 @@
+;; # Testsets
+;; 
+;; Für die Analyse stehen uns zwei Testsets an Initien zur Verfügung.
+;; 
+;; Das erste Testset besteht aus 11 Initien aus dem Altbestand sowie 
+;; 39 Initien, welche mit bestimmten Mutationen aus den ersten Initien
+;; generiert wurden. 
+;; 
+;; Das zweite Testset besteht aus 100 Initien aus dem Altbestand
+
+
+(ns notebooks.index
+  {:nextjournal.clerk/visibility {:code :hide}}
+  (:require
+    [nextjournal.clerk :as clerk]))
+
+;; ## Testset 1
+
+(clerk/md "...to continue")
+
+;; ## Testset 2
+
+(clerk/md "...to continue")
