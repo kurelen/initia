fcc68a953e7baa214276076ed00535edad80e7da
Alexander Jandt <kurelen@pm.me>
2025-07-29 06:50:18 +0200

    Fix data test

--- test/initia/data_test.clj
+++ test/initia/data_test.clj
@@ -1,11 +1,12 @@
 (ns initia.data-test
   (:require
-    [clojure.test :refer [deftest is testing]]))
+    [clojure.test :refer [deftest is testing]]
+    [initia.data :as data]))
 
 
 (deftest test-fixtures
   (testing "testing dataset 1"
-    (is (= true true)))
+    (is (= 50 (count data/testset-1))))
 
   (testing "testing dataset 2"
-    (is (= true true))))
+    (is (= 100 (count data/testset-2)))))
