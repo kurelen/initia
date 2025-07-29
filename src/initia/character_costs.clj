77abee6929b0eea1a7062b6cfb1b017f96282f1f
Alexander Jandt <kurelen@pm.me>
2025-07-29 13:16:58 +0200

    Add character cost helper

---
+++ src/initia/character_costs.clj
@@ -0,0 +1,23 @@
+(ns initia.character-costs
+  (:require
+    [clojure.string :refer [lower-case]]
+    [initia.data :as data]))
+
+
+(defn create-substitution-cost-fn
+  "Creates a character substitution cost function from a cost lookup table."
+  [cost-lookup]
+  (fn [a b]
+    (let [c (lower-case a)
+          d (lower-case b)]
+      (double (or (and (= c d) 0.0)
+                  (cost-lookup [c d])
+                  (cost-lookup [d c])
+                  1.0)))))
+
+
+(def medieval-cost-fn
+  "Pre-configured character substitution costs for medieval manuscripts."
+  {:substitute
+   (create-substitution-cost-fn
+     (:substitute data/weights))})
